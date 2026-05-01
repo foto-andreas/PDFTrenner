2470176a32f531b965d0fb70d4b10de821c5ccaf
Andreas Schrell <as@schrell.de>
2026-05-01 23:55:35 +0200

    Quit app when window closes; remove .app bundle from repo
    - Add applicationShouldTerminateAfterLastWindowClosed returning true
    - Remove PDFTrenner.app from git (build artifact, now in .gitignore)

--- PDFTrennerSwift/PDFTrennerSwift/PDFTrennerApp.swift
+++ PDFTrennerSwift/PDFTrennerSwift/PDFTrennerApp.swift
@@ -20,4 +20,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.activate(ignoringOtherApps: true)
     }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
